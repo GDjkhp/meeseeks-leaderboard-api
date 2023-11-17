af4b72627bfca6b3d2717f806178da0584547567
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2023-11-17 10:59:16 +0800

    force stop
    mobile hang fix?

--- meeseeks-script.js
+++ meeseeks-script.js
@@ -3,8 +3,8 @@ var result, page = 0, serverId, queue, queueLimit, previousQueue = null, update 
 // cors unblocked api server, please don't abuse (rate limited, can ban my server's ip address), delays 500ms
 async function loadJSON(id) {
     if (turing) return;
-    // result = await fetch(`https://meeseeks-api.gdjkhp.repl.co/${id}?limit=1000&page=${page}`).then(res => res.json());
-    result = await fetch(`https://cors-anywhere.gdjkhp.repl.co/mee6.xyz/api/plugins/levels/leaderboard/${id}?limit=1000&page=${page}`).then(res => res.json());
+    result = await fetch(`https://meeseeks-api.gdjkhp.repl.co/${id}?limit=1000&page=${page}`).then(res => res.json());
+    // result = await fetch(`https://cors-anywhere.gdjkhp.repl.co/mee6.xyz/api/plugins/levels/leaderboard/${id}?limit=1000&page=${page}`).then(res => res.json());
     // result = await fetch(`https://cors.gdjkhp.repl.co/mee6.xyz/api/plugins/levels/leaderboard/${id}?limit=1000&page=${page}`).then(res => res.json());
     console.log(result);
     if (page == 0) topXP = result.players[0].xp;
@@ -438,8 +438,8 @@ function reset() {
 // real main
 async function parseReal(server, input) {
     try {
-        await parseServer(server);
         stop1.disabled = false;
+        await parseServer(server);
         await parseInput(input);
         parseButton.value = "Parse";
     } catch (error) {
